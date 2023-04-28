8967f52bb85d612e7e014b7dfc4c87e8b9da27f6
Ivan G <mr.zero@inbox.ru>
2023-04-28 18:48:58 +0300

    feat: add file with English layout

---
+++ modules/layoutUS.js
@@ -0,0 +1,296 @@
+export default [
+  {
+    code: 'Backquote',
+    lowerCase: '`',
+    upperCase: '~',
+  },
+  {
+    code: 'Digit1',
+    lowerCase: '1',
+    upperCase: '!',
+  },
+  {
+    code: 'Digit2',
+    lowerCase: '2',
+    upperCase: '@',
+  },
+  {
+    code: 'Digit3',
+    lowerCase: '3',
+    upperCase: '#',
+  },
+  {
+    code: 'Digit4',
+    lowerCase: '4',
+    upperCase: '$',
+  },
+  {
+    code: 'Digit5',
+    lowerCase: '5',
+    upperCase: '%',
+  },
+  {
+    code: 'Digit6',
+    lowerCase: '6',
+    upperCase: '^',
+  },
+  {
+    code: 'Digit7',
+    lowerCase: '7',
+    upperCase: '&',
+  },
+  {
+    code: 'Digit8',
+    lowerCase: '8',
+    upperCase: '*',
+  },
+  {
+    code: 'Digit9',
+    lowerCase: '9',
+    upperCase: '(',
+  },
+  {
+    code: 'Digit0',
+    lowerCase: '0',
+    upperCase: ')',
+  },
+  {
+    code: 'Minus',
+    lowerCase: '-',
+    upperCase: '_',
+  },
+  {
+    code: 'Equal',
+    lowerCase: '=',
+    upperCase: '+',
+  },
+  {
+    code: 'Backspace',
+    lowerCase: 'Backspace',
+    upperCase: 'Backspace',
+  },
+  {
+    code: 'Tab',
+    lowerCase: 'Tab',
+    upperCase: 'Tab',
+  },
+  {
+    code: 'KeyQ',
+    lowerCase: 'q',
+  },
+  {
+    code: 'KeyW',
+    lowerCase: 'w',
+  },
+  {
+    code: 'KeyE',
+    lowerCase: 'e',
+  },
+  {
+    code: 'KeyR',
+    lowerCase: 'r',
+  },
+  {
+    code: 'KeyT',
+    lowerCase: 't',
+  },
+  {
+    code: 'KeyY',
+    lowerCase: 'y',
+  },
+  {
+    code: 'KeyU',
+    lowerCase: 'u',
+  },
+  {
+    code: 'KeyI',
+    lowerCase: 'i',
+  },
+  {
+    code: 'KeyO',
+    lowerCase: 'o',
+  },
+  {
+    code: 'KeyP',
+    lowerCase: 'p',
+  },
+  {
+    code: 'BracketLeft',
+    lowerCase: '[',
+    upperCase: '{',
+  },
+  {
+    code: 'BracketRight',
+    lowerCase: ']',
+    upperCase: '}',
+  },
+  {
+    code: 'Backslash',
+    lowerCase: '\\',
+    upperCase: '|',
+  },
+  {
+    code: 'Delete',
+    lowerCase: 'Delete',
+    upperCase: 'Delete',
+  },
+  {
+    code: 'CapsLock',
+    lowerCase: 'CapsLock',
+    upperCase: 'CapsLock',
+  },
+  {
+    code: 'KeyA',
+    lowerCase: 'a',
+  },
+  {
+    code: 'KeyS',
+    lowerCase: 's',
+  },
+  {
+    code: 'KeyD',
+    lowerCase: 'd',
+  },
+  {
+    code: 'KeyF',
+    lowerCase: 'f',
+  },
+  {
+    code: 'KeyG',
+    lowerCase: 'g',
+  },
+  {
+    code: 'KeyH',
+    lowerCase: 'h',
+  },
+  {
+    code: 'KeyJ',
+    lowerCase: 'j',
+  },
+  {
+    code: 'KeyK',
+    lowerCase: 'k',
+  },
+  {
+    code: 'KeyL',
+    lowerCase: 'l',
+  },
+  {
+    code: 'Semicolon',
+    lowerCase: ';',
+    upperCase: ':',
+  },
+  {
+    code: 'Quote',
+    lowerCase: '\'',
+    upperCase: '"',
+  },
+  {
+    code: 'Enter',
+    lowerCase: 'Enter',
+    upperCase: 'Enter',
+  },
+  {
+    code: 'ShiftLeft',
+    lowerCase: 'Shift',
+    upperCase: 'Shift',
+  },
+  {
+    code: 'KeyZ',
+    lowerCase: 'z',
+  },
+  {
+    code: 'KeyX',
+    lowerCase: 'x',
+  },
+  {
+    code: 'KeyC',
+    lowerCase: 'c',
+  },
+  {
+    code: 'KeyV',
+    lowerCase: 'v',
+  },
+  {
+    code: 'KeyB',
+    lowerCase: 'b',
+  },
+  {
+    code: 'KeyN',
+    lowerCase: 'n',
+  },
+  {
+    code: 'KeyM',
+    lowerCase: 'm',
+  },
+  {
+    code: 'Comma',
+    lowerCase: ',',
+    upperCase: '<',
+  },
+  {
+    code: 'Period',
+    lowerCase: '.',
+    upperCase: '>',
+  },
+  {
+    code: 'Slash',
+    lowerCase: '/',
+    upperCase: '?',
+  },
+  {
+    code: 'ArrowUp',
+    lowerCase: '↑',
+    upperCase: '↑',
+  },
+  {
+    code: 'ShiftRight',
+    lowerCase: 'Shift',
+    upperCase: 'Shift',
+  },
+  {
+    code: 'ControlLeft',
+    lowerCase: 'Ctrl',
+    upperCase: 'Ctrl',
+  },
+  {
+    code: 'MetaLeft',
+    lowerCase: '⊞',
+    upperCase: '⊞',
+  },
+  {
+    code: 'AltLeft',
+    lowerCase: 'Alt',
+    upperCase: 'Alt',
+  },
+  {
+    code: 'Space',
+    lowerCase: ' ',
+    upperCase: ' ',
+  },
+  {
+    code: 'AltRight',
+    lowerCase: 'Alt',
+    upperCase: 'Alt',
+  },
+  {
+    code: 'ArrowLeft',
+    lowerCase: '←',
+    upperCase: '←',
+  },
+  {
+    code: 'ArrowDown',
+    lowerCase: '↓',
+    upperCase: '↓',
+  },
+  {
+    code: 'ArrowRight',
+    lowerCase: '→',
+    upperCase: '→',
+  },
+  {
+    code: 'ControlRight',
+    lowerCase: 'Ctrl',
+    upperCase: 'Ctrl',
+  },
+];
